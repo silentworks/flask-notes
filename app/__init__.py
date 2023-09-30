56304f3e4efb05235608b4eda29d6cc8ce1a8b5c
Andrew Smith <a.smith@silentworks.co.uk>
2023-09-30 23:16:11 +0000

    Update with single note view

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask, g, render_template
+from PIL import Image, ImageOps
 from app.supabase import (
+    supabase,
     session_context_processor,
     get_profile_by_slug,
     get_profile_by_user,
+    get_all_notes_by_user_id,
+    get_note_by_slug,
 )
 from app.decorators import login_required, password_update_required, profile_required
 from app.auth import auth
 from app.account import account
 from app.notes import notes
+from app.utils import resize_image
 
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
@@ -36,7 +41,26 @@ def home():
 @app.route("/u/<slug>")
 def u(slug):
     profile = get_profile_by_slug(slug)
-    return render_template("profile.html", profile=profile)
+    notes = get_all_notes_by_user_id(profile.get("id"))
+    return render_template("profile.html", profile=profile, notes=notes)
+
+
+@app.route("/u/<slug>/<note_slug>")
+def user_note(slug, note_slug):
+    profile = get_profile_by_slug(slug)
+    note = get_note_by_slug(note_slug)
+    featured_image = None
+    try:
+        r = supabase.storage.from_("featured_image").get_public_url(
+            note["featured_image"]
+        )
+        featured_image = resize_image(r, 700, 400)
+    except:
+        None
+
+    return render_template(
+        "note.html", profile=profile, note=note, featured_image=featured_image
+    )
 
 
 @app.route("/service-unavailable")
